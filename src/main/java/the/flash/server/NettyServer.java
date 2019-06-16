2c55c66e7f53bf99a7e5e1fc70efcaa4ade5887e
wuwenbin <wuwenbin@daojia-inc.com>
2019-06-16 16:20:24 +0800

    代码注释

--- src/main/java/the/flash/server/NettyServer.java
+++ src/main/java/the/flash/server/NettyServer.java
@@ -12,16 +12,23 @@ public class NettyServer {
     private static final int PORT = 8000;
 
     public static void main(String[] args) {
+        //bossGroup表示监听端口，accept 新连接的线程组，workerGroup表示处理每一条连接的数据读写的线程组
         NioEventLoopGroup boosGroup = new NioEventLoopGroup();
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
         final ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap
                 .group(boosGroup, workerGroup)
+                //服务端的 IO 模型为NIO
                 .channel(NioServerSocketChannel.class)
+                //表示系统用于临时存放已完成三次握手的请求的队列的最大长度
                 .option(ChannelOption.SO_BACKLOG, 1024)
+                //表示是否开启TCP底层心跳机制
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
+                //表示是否开启Nagle算法，true表示关闭，false表示开启，通俗地说，如果要求高实时性，有数据发送时就马上发送，就关闭，
+                // 如果需要减少发送次数减少网络交互，就开启。
                 .childOption(ChannelOption.TCP_NODELAY, true)
+                //给这个引导类创建一个ChannelInitializer，这里主要就是定义后续每条连接的数据读写，业务处理逻
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     protected void initChannel(NioSocketChannel ch) {
                         ch.pipeline().addLast(new FirstServerHandler());
@@ -38,6 +45,7 @@ public class NettyServer {
                 System.out.println("端口[" + port + "]绑定成功!");
             } else {
                 System.err.println("端口[" + port + "]绑定失败!");
+               // bind(serverBootstrap,port+1);
             }
         });
     }
